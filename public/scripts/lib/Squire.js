b3b73751f06faa50bb951e46f96a42c9c0afac5a
giles <giles.bradshaw@sigyl.com>
2015-06-29 22:53:47 +0100

    gulp-util

--- public/scripts/lib/Squire.js
+++ public/scripts/lib/Squire.js
@@ -1,10 +1,4 @@
-define([], function() {
-  var Squire = function(){
-
-  };
-  Squire.prototype.clean = function (){
-    console.log("cleaned");
-
-  };
-  return Squire; 
+define([], function(){
+    return 'ok';
 });
+
