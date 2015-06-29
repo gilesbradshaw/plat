7ca513cdf9e8e3e36a3a299b898343fd34b22a51
giles <giles.bradshaw@sigyl.com>
2015-06-29 23:04:46 +0100

    just ok

--- public/scripts/lib/Squire.js
+++ public/scripts/lib/Squire.js
@@ -1,14 +1,4 @@
 
 define([], function() {
    return 'ok';
-  var Squire = function(){
-
-  };
-  
- 
-  Squire.prototype.clean = function (){
-    console.log("cleaned");
-
-  };
-  return Squire; 
 });
